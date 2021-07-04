5a894e2e94aa0c2345c42c4a15a6d3caeb4c7ad2
zjx20 <zhoujianxiong2@gmail.com>
2021-07-04 22:09:22 +0800

    Update install_catwalk.sh

--- install_catwalk.sh
+++ install_catwalk.sh
@@ -31,10 +31,10 @@ tar xvf "$file_name"
 sudo cp server /usr/local/bin/catwalk_server
 
 sudo mkdir -p /var/lib/catwalk
-chmod 755 /var/lib/catwalk
-touch /var/lib/catwalk/conf_server.json
-echo "{}" > /var/lib/catwalk/conf_server.json
-chmod 755 /var/lib/catwalk/conf_server.json
+sudo chmod 755 /var/lib/catwalk
+sudo touch /var/lib/catwalk/conf_server.json
+sudo echo "{}" > /var/lib/catwalk/conf_server.json
+sudo chmod 755 /var/lib/catwalk/conf_server.json
 
 sudo wget -O /etc/systemd/system/catwalk.service https://raw.githubusercontent.com/zjx20/catwalk_deploy/main/catwalk.service
 
